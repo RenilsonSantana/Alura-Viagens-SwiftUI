e0e1cb62c19daaef8b901128f01d3a6bc5dd886a
Renilson Santana Santos <renilson_bmn@hotmail.com>
2021-05-21 12:50:30 -0300

    Initial Commit

---
+++ Alura Viagens SwiftUI/Alura_Viagens_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alura_Viagens_SwiftUIApp.swift
+//  Alura Viagens SwiftUI
+//
+//  Created by Renilson Santana on 21/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Alura_Viagens_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
